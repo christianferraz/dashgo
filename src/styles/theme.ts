705473f41ca6e0e74105696b0a77e137bd84745a
Christian Ferraz <christianferraz@gmail.com>
2022-04-28 02:10:53 -0400

    Aula seçao 3

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+  colors: {
+    "900": "#181b23"
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.900",
+        color: "gray.50"
+      }
+    }
+  }
+})
